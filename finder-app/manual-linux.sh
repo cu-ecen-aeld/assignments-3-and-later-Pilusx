2f1409ec9f46911f5d7a5794c7077324fb69a14f
pilusx <poteka@wp.pl>
2025-10-27 15:59:02 +0100

    Assignment 3 - part 2

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -17,7 +17,7 @@ if [ $# -lt 1 ]
 then
 	echo "Using default directory ${OUTDIR} for output"
 else
-	OUTDIR=$1
+	OUTDIR=$(realpath $1)
 	echo "Using passed directory ${OUTDIR} for output"
 fi
 
@@ -35,16 +35,23 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     git checkout ${KERNEL_VERSION}
 
     # TODO: Add your kernel build steps here
+    # sudo apt-get -y install libncurses-dev libssl-dev libelf-dev qemu-system
+    make mrproper
+    #make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} menuconfig
+    cp -f ${FINDER_APP_DIR}/../conf/linux_config .config
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} -j6 all
+    cd ..
 fi
 
 echo "Adding the Image in outdir"
+cp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}/
 
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
 if [ -d "${OUTDIR}/rootfs" ]
 then
 	echo "Deleting rootfs directory at ${OUTDIR}/rootfs and starting over"
-    sudo rm  -rf ${OUTDIR}/rootfs
+    sudo rm -rf ${OUTDIR}/rootfs
 fi
 
 # TODO: Create necessary base directories
@@ -61,20 +68,48 @@ else
 fi
 
 # TODO: Make and install busybox
+mkdir -p ${OUTDIR}/rootfs/bin
+if [ ! -f ${OUTDIR}/rootfs/bin/busybox ]; then
+    #make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} menuconfig
+    cp -f ${FINDER_APP_DIR}/../conf/busy_config .config
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} -j6
+    make CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
+    cd ${OUTDIR}
+fi
+
+cd ${OUTDIR}/rootfs
+mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
+mkdir -p usr/bin usr/lib usr/sbin
+mkdir -p var/log
 
 echo "Library dependencies"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
 # TODO: Add library dependencies to rootfs
+for lib in "lib/ld-linux-aarch64.so.1" "lib64/libm.so.6" "lib64/libresolv.so.2" "lib64/libc.so.6"; do
+    cp $(${CROSS_COMPILE}gcc -print-file-name=$(basename $lib)) ./${lib}
+done
 
 # TODO: Make device nodes
+sudo mknod -m 666 dev/null c 1 3
+sudo mknod -m 666 dev/console c 5 1
 
 # TODO: Clean and build the writer utility
+cd ${FINDER_APP_DIR}
+make CROSS_COMPILE=${CROSS_COMPILE} clean writer
 
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
+cp -rL ${FINDER_APP_DIR}/* ${OUTDIR}/rootfs/home
+cp -r ${FINDER_APP_DIR}/../conf ${OUTDIR}/rootfs
 
 # TODO: Chown the root directory
+sudo chown -R root:root ${OUTDIR}/rootfs
 
 # TODO: Create initramfs.cpio.gz
+cd ${OUTDIR}/rootfs
+find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
+cd ${OUTDIR}
+gzip -f initramfs.cpio
+
